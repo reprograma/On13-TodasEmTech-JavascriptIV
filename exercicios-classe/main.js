f68490a78c7ef2501ba0473465858cba8d62b3df
Aline Viana <alinemaciel018@gmail.com>
2021-10-23 13:56:35 -0300

    first commit

---
+++ exercicios-classe/main.js
@@ -0,0 +1,22 @@
+function trazerPersonagens () {
+    const myHeaders = new Headers();
+    
+/*     myHeaders.append("Content-type", "JSON");
+    myHeaders.append("Authorization", "asasasasassasasa"); */
+    
+    const myInit = {
+        method: 'GET',
+        headers: myHeaders,
+    }
+
+
+    fetch("https://adventuretimeapi.herokuapp.com/people", myInit)
+    .then((resposta) => {
+        return resposta.text()
+    })
+    .then((resultado) => console.log(resultado))
+    .catch((error)=> {
+        console.log(error)
+    })
+
+}
