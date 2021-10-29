6c269f3049fb261809de9e626bafa2920de1689f
Ylana Portela <ylanaportela2000@gmail.com>
2021-10-29 17:45:28 -0300

    Adicionando arquivos dos exercícios feitos na aula

---
+++ assets/exercicio-classe/exercicio-post/main.js
@@ -0,0 +1,25 @@
+function inserirAutentificacao() {
+    const myHeaders = new Headers();
+
+    myHeaders.append("Content-Type", "application/json");
+
+    const myBody = JSON.stringify({
+        "username": "admin",
+        "password": "password"
+    });
+
+    const myInit = {
+        method: 'POST',
+        headers: myHeaders,
+        body: myBody,
+        mode: 'cors'
+    }
+
+    fetch("https://automationintesting.online/auth/login", myInit)
+        .then((response) => {
+            return response.status
+        })
+        .then((resultado) =>{
+            console.log(resultado)
+        })
+}
