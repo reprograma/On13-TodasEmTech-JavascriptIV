6c9de4aaa6f484460c0780e221ee1191328f3a59
unknown <claudyaagro@yahoo.com.br>
2021-10-29 19:56:18 -0300

    Atividade da semana

---
+++ main.js
@@ -0,0 +1,15 @@
+function inserirAutenticacao({
+    const myHeaders = new Headers();
+
+
+    const myBody = JSON.stringify({
+        "username": "admin",
+        "password": "password",
+
+    })
+    const myInit = {
+        method
+    }
+
+
+})
