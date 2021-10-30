bcd92caea314d68d7353e8b29e6a4931490c2921
Taina <taiaguilera@gmail.com>
2021-10-30 19:14:53 -0300

    entregando o exercício respondido

--- Exercicio Post/main.js
+++ Exercicio Post/main.js
@@ -1,8 +1,8 @@
 function trazerPersonagens () {
 const myHeaders = new Headers ();
 const myInit = {
-method: 'GET',
-Headers: myHeaders,
+    method: 'GET',
+    Headers: myHeaders,
 }
 fetch("https://borgesdn.github.io/pokedex-source/pokedex.json")
 
